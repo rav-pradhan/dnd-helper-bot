857179d68c60cac72ed2ca5eb3920bd7c7a96b9d
Ravi <ravipradhan@protonmail.com>
2020-06-08 20:35:40 +0100

    add terser plugin for minification build

---
+++ rollup.config.js
@@ -0,0 +1,9 @@
+// rollup.config.js
+import { terser } from "rollup-plugin-terser";
+
+export default {
+  input: "src/index.js",
+  output: [
+    { file: "dist/bundle.js", format: "cjs", plugins: [terser()] },
+  ],
+};
--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 require('dotenv').config();
 import * as Discord from 'discord.js'
 import { themes } from './themes/themes'
+import dotenv from 'dotenv'
 
+dotenv.config()
 const client = new Discord.Client();
 
 client.once('ready', () => {
