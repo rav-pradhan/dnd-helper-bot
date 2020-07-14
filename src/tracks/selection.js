c69c96e5c31f6316b0391eefc903e3995b5f22f5
Ravi <ravipradhan@protonmail.com>
2020-07-14 15:43:30 +0100

    update track list

--- src/tracks/selection.js
+++ src/tracks/selection.js
@@ -1,13 +1,25 @@
 export const tracks = {
-  ambientCave: ['https://www.youtube.com/watch?v=E72yDpAfrgY'],
-  ambientForest: ['https://youtu.be/d0tU18Ybcvk'],
-  forest: ['https://www.youtube.com/watch?v=83X7bNjkUWs', 'https://www.youtube.com/watch?v=DiCGgN4gJ8s'],
-  battle: ['https://www.youtube.com/watch?v=lAGm9MTyRJ8'],
-  rob: ['https://www.youtube.com/watch?v=dIiZ3vvZ78s'],
-  boss: [
-    'https://www.youtube.com/watch?v=7sGMKm1Ozfw',
-    'https://www.youtube.com/watch?v=ZIF-VbKfnxc'
-  ],
-  town: ['https://www.youtube.com/watch?v=xu2pESvXcmM'],
-  victory: ['https://www.youtube.com/watch?v=-YCN-a0NsNk']
+    ambientCave: ['https://www.youtube.com/watch?v=E72yDpAfrgY'],
+    ambientForest: ['https://youtu.be/d0tU18Ybcvk'],
+    forest: ['https://www.youtube.com/watch?v=83X7bNjkUWs', 'https://www.youtube.com/watch?v=DiCGgN4gJ8s'],
+    battle: ['https://www.youtube.com/watch?v=lAGm9MTyRJ8'],
+    cave: ['https://www.youtube.com/watch?v=zO85DZSQEbw'],
+    dungeon: ['https://www.youtube.com/watch?v=6Gg6meoG_98', 'https://youtu.be/YTYkUvaEsio'],
+    glitch: ['https://www.youtube.com/watch?v=5y_9P1y7V98'],
+    evil: ['https://youtu.be/lf7qedGXOZg'],
+    rob: ['https://www.youtube.com/watch?v=dIiZ3vvZ78s'],
+    boss: [
+        'https://www.youtube.com/watch?v=7sGMKm1Ozfw',
+        'https://www.youtube.com/watch?v=5R5GCUOglt8'
+    ],
+    town: ['https://www.youtube.com/watch?v=xu2pESvXcmM'],
+    victory: ['https://www.youtube.com/watch?v=-YCN-a0NsNk'],
+    mi: ['https://www.youtube.com/watch?v=kmBM3sYpF1o&feature=youtu.be'],
+    heist: ['https://www.youtube.com/watch?v=H-vhDD7gDRM'],
+    coast: ['https://www.youtube.com/watch?v=4W2EfqdOmiI'],
+    kotch: ['https://www.youtube.com/watch?v=5Je4HgOMQ3I'],
+    broadsteppes: ['https://youtu.be/_52K0E_gNY0'],
+    seaside: ['https://youtu.be/mMSa-3xSKk0'],
+    mansion: ['https://youtu.be/LgtRqwfJGbI'],
+
 }
